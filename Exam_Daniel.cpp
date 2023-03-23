3c71fb13fbb38e9624acf3e0948e7e20d34a5d7a
Shlmp <daniel.kil@uniat.edu.mx>
2023-03-23 10:55:03 -0600

    Added switch

--- Exam_Daniel.cpp
+++ Exam_Daniel.cpp
@@ -1,11 +1,24 @@
 // Exam_Daniel.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 
-
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int parcial;
+    cout << "Ingrese el parcial que desea checar!\n";
+	cin >> parcial;
+
+	switch (parcial)
+	{
+		case 1:
+			cout << "hola";
+		break;
+	default:
+		break;
+	}
+	return 0;
 }
 
 
